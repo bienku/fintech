f1a58ae5e0c1e517f8dc2cac126d5a0d87004cbd
Krzysztof Bienias <krzysztofzbienias@gmail.com>
2024-06-12 17:14:56 +0200

    feat: Add entry page and update layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -9,11 +9,6 @@ export {
     ErrorBoundary,
 } from 'expo-router';
 
-export const unstable_settings = {
-    // Ensure that reloading on `/modal` keeps a back button present.
-    initialRouteName: '(tabs)',
-};
-
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
@@ -42,5 +37,9 @@ export default function RootLayout() {
 }
 
 function RootLayoutNav() {
-    return <Stack></Stack>;
+    return (
+        <Stack>
+            <Stack.Screen name='index' options={{ headerShown: false }} />
+        </Stack>
+    );
 }
